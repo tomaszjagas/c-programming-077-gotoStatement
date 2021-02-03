f0a536156793e82a4dc3e60eb3d7bb1e9fe6f802
Tomasz Jagas <tomasz.jagas@gmail.com>
2021-02-03 22:09:19 +0100

    first commit

---
+++ c-programming-077-gotoStatement.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main(void) {
+    const int maxInput = 5;
+    int i = 0;
+    double number, average, sum = 0.0;
+
+    for (i=1; i<=maxInput; ++i) {
+        printf("%d. Enter a number: ", i);
+        scanf("%lf", &number);
+
+        if  (number<0.0) {
+            goto jump;
+        }
+
+        sum += number;
+    }
+
+    jump:
+        average = sum/(i-1);
+        printf("Sum = %.2f\n", sum);
+        printf("Average = %.2f\n", average);
+
+    return 0;
+}
